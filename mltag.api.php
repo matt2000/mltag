6301e3a8713053978b46ae562893ce47db09223e
arjkap <arjkap@gmail.com>
2012-12-19 19:10:09 +0530

    Removed coding errors

--- mltag.api.php
+++ mltag.api.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * @file
  * Still under development. Will commit this soon
@@ -10,10 +10,10 @@
  * Define various algorithms used throughout this project.
  */
 function hook_mltag_custom_algorithms() {
-  return array(    
+  return array(
     'mymodule_first_algo' => array(
-        'label' => 'My first awesome algorithm',
-        'description' => 'This algorithm selects terms based on a ranking of their awesomeness.',
+      'label' => 'My first awesome algorithm',
+      'description' => 'This algorithm selects terms based on a ranking of their awesomeness.',
     ),
     'mymodule_other_algo' => array('label' => 'My other mysterious algorithm',
     ),
